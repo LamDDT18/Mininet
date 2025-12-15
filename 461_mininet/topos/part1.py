993eb6de2f83d2608edf20dfaf73132d02759ef2
LamDDT18 <ddtlam000@gmail.com>
2025-12-15 22:39:50 +0700

    Update part1.py

--- 461_mininet/topos/part1.py
+++ 461_mininet/topos/part1.py
@@ -10,16 +10,16 @@ from mininet.node import Controller
 class part1_topo(Topo):
     def build(self):
 
-        # add switch
+        #switch
         s1 = self.addSwitch('s1')
 
-        # add hosts
+        #hosts
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
         h3 = self.addHost('h3')
         h4 = self.addHost('h4')
 
-        # add links
+        #links
         self.addLink(h1, s1)
         self.addLink(h2, s1)
         self.addLink(h3, s1)
