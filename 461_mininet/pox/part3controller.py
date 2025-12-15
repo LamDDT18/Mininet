e8f9f365b32e7ef13abce0d41038e2bb2a43883a
LamDDT18 <ddtlam000@gmail.com>
2025-12-15 16:24:35 +0700

    Update part3controller.py

--- 461_mininet/pox/part3controller.py
+++ 461_mininet/pox/part3controller.py
@@ -1,115 +1,65 @@
-# Part 3 of UWCSE's Mininet-SDN project
-#
-# based on Lab Final from UCSC's Networking Class
-# which is based on of_tutorial by James McCauley
-
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
-from pox.lib.addresses import IPAddr, IPAddr6, EthAddr
+from pox.lib.packet import ethernet, ipv4
 
 log = core.getLogger()
 
-# Convenience mappings of hostnames to ips
-IPS = {
-    "h10": "10.0.1.10",
-    "h20": "10.0.2.20",
-    "h30": "10.0.3.30",
-    "serv1": "10.0.4.10",
-    "hnotrust": "172.16.10.100",
-}
-
-# Convenience mappings of hostnames to subnets
-SUBNETS = {
-    "h10": "10.0.1.0/24",
-    "h20": "10.0.2.0/24",
-    "h30": "10.0.3.0/24",
-    "serv1": "10.0.4.0/24",
-    "hnotrust": "172.16.10.0/24",
-}
-
+CORE_DPID = 21
 
 class Part3Controller(object):
-    """
-    A Connection object for that switch is passed to the __init__ function.
-    """
-
     def __init__(self, connection):
-        print(connection.dpid)
-        # Keep track of the connection to the switch so that we can
-        # send it messages!
         self.connection = connection
-
-        # This binds our PacketIn event listener
         connection.addListeners(self)
-        # use the dpid to figure out what switch is being created
-        if connection.dpid == 1:
-            self.s1_setup()
-        elif connection.dpid == 2:
-            self.s2_setup()
-        elif connection.dpid == 3:
-            self.s3_setup()
-        elif connection.dpid == 21:
-            self.cores21_setup()
-        elif connection.dpid == 31:
-            self.dcs31_setup()
-        else:
-            print("UNKNOWN SWITCH")
-            exit(1)
 
-    def s1_setup(self):
-        # put switch 1 rules here
-        pass
+        dpid = connection.dpid
 
-    def s2_setup(self):
-        # put switch 2 rules here
-        pass
-
-    def s3_setup(self):
-        # put switch 3 rules here
-        pass
-
-    def cores21_setup(self):
-        # put core switch rules here
-        pass
-
-    def dcs31_setup(self):
-        # put datacenter switch rules here
-        pass
-
-    # used in part 4 to handle individual ARP packets
-    # not needed for part 3 (USE RULES!)
-    # causes the switch to output packet_in on out_port
-    def resend_packet(self, packet_in, out_port):
-        msg = of.ofp_packet_out()
-        msg.data = packet_in
-        action = of.ofp_action_output(port=out_port)
-        msg.actions.append(action)
-        self.connection.send(msg)
-
-    def _handle_PacketIn(self, event):
-        """
-        Packets not handled by the router rules will be
-        forwarded to this method to be handled by the controller
-        """
-
-        packet = event.parsed  # This is the parsed packet data.
-        if not packet.parsed:
-            log.warning("Ignoring incomplete packet")
+        # Non-core switches: flood everything
+        if dpid != CORE_DPID:
+            msg = of.ofp_flow_mod()
+            msg.priority = 1
+            msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+            connection.send(msg)
             return
 
-        packet_in = event.ofp  # The actual ofp_packet_in message.
-        print(
-            "Unhandled packet from " + str(self.connection.dpid) + ":" + packet.dump()
-        )
+        # ---------- CORE SWITCH (cores21) ----------
 
+        # Block ICMP from hnotrust1 to internal + server
+        for dst in ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.10/32"]:
+            msg = of.ofp_flow_mod()
+            msg.priority = 200
+            msg.match.dl_type = ethernet.IP_TYPE
+            msg.match.nw_proto = ipv4.ICMP_PROTOCOL
+            msg.match.nw_src = "172.16.10.100"
+            msg.match.nw_dst = dst
+            connection.send(msg)
+
+        # Block all IP from hnotrust1 to serv1
+        msg = of.ofp_flow_mod()
+        msg.priority = 190
+        msg.match.dl_type = ethernet.IP_TYPE
+        msg.match.nw_src = "172.16.10.100"
+        msg.match.nw_dst = "10.0.4.10"
+        connection.send(msg)
+
+        # Forwarding rules (static)
+        routes = {
+            "10.0.1.0/24": 1,
+            "10.0.2.0/24": 2,
+            "10.0.3.0/24": 3,
+            "10.0.4.0/24": 4,
+            "172.16.10.0/24": 5
+        }
+
+        for subnet, port in routes.items():
+            msg = of.ofp_flow_mod()
+            msg.priority = 10
+            msg.match.dl_type = ethernet.IP_TYPE
+            msg.match.nw_dst = subnet
+            msg.actions.append(of.ofp_action_output(port=port))
+            connection.send(msg)
 
 def launch():
-    """
-    Starts the component
-    """
-
     def start_switch(event):
-        log.debug("Controlling %s" % (event.connection,))
         Part3Controller(event.connection)
 
     core.openflow.addListenerByName("ConnectionUp", start_switch)
