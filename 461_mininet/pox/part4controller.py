f988ee96291ec24a7bba117f4dd26bbd989fb5e1
LamDDT18 <ddtlam000@gmail.com>
2025-12-15 21:38:49 +0700

    Update part4controller.py

--- 461_mininet/pox/part4controller.py
+++ 461_mininet/pox/part4controller.py
@@ -5,55 +5,46 @@ from pox.lib.addresses import EthAddr, IPAddr
 
 log = core.getLogger()
 
-HNOTRUST_IP = IPAddr("172.16.10.100")
-SERV1_IP = IPAddr("10.0.4.10")
-
 ROUTER_MAC = EthAddr("00:00:00:aa:bb:cc")
 
-GATEWAYS = {
-    IPAddr("10.0.1.1"),
-    IPAddr("10.0.2.1"),
-    IPAddr("10.0.3.1"),
-    IPAddr("10.0.4.1"),
-    IPAddr("172.16.10.1")
-}
+HNOTRUST = IPAddr("172.16.10.100")
+SERV1    = IPAddr("10.0.4.10")
 
 class Part4Router(object):
     def __init__(self, connection):
         self.connection = connection
-        self.arp_table = {}  # IP -> (MAC, port)
+        self.arp_table = {}   # ip -> (mac, port)
         connection.addListeners(self)
 
     def _handle_PacketIn(self, event):
-        packet = event.parsed
+        pkt = event.parsed
         in_port = event.port
 
-        if packet.type == ethernet.ARP_TYPE:
-            self._handle_arp(packet, in_port)
-            return
+        if pkt.type == ethernet.ARP_TYPE:
+            self.handle_arp(pkt, in_port)
+        elif pkt.type == ethernet.IP_TYPE:
+            self.handle_ip(pkt, event, in_port)
 
-        if packet.type == ethernet.IP_TYPE:
-            self._handle_ip(packet, event, in_port)
-
-    def _handle_arp(self, packet, in_port):
-        a = packet.payload
+    # ---------------- ARP ----------------
+    def handle_arp(self, pkt, in_port):
+        a = pkt.payload
 
         # Learn sender
-        self.arp_table[a.protosrc] = (packet.src, in_port)
+        self.arp_table[a.protosrc] = (pkt.src, in_port)
 
-        # Reply if ARP is for router gateway
-        if a.opcode == arp.REQUEST and a.protodst in GATEWAYS:
+        # Reply if ARP for gateway
+        if a.opcode == arp.REQUEST:
             reply = arp()
             reply.opcode = arp.REPLY
             reply.hwsrc = ROUTER_MAC
-            reply.hwdst = packet.src
+            reply.hwdst = pkt.src
             reply.protosrc = a.protodst
             reply.protodst = a.protosrc
 
             eth = ethernet(
                 type=ethernet.ARP_TYPE,
                 src=ROUTER_MAC,
-                dst=packet.src,
+                dst=pkt.src,
                 payload=reply
             )
 
@@ -62,47 +53,49 @@ class Part4Router(object):
             msg.actions.append(of.ofp_action_output(port=in_port))
             self.connection.send(msg)
 
-    def _handle_ip(self, packet, event, in_port):
-        ip = packet.payload
+    # ---------------- IP ----------------
+    def handle_ip(self, pkt, event, in_port):
+        ip = pkt.payload
 
-        # ---------- SECURITY POLICIES ----------
-        if ip.srcip == HNOTRUST_IP:
-            if ip.dstip == SERV1_IP:
+        # ---- SECURITY POLICIES ----
+        if ip.srcip == HNOTRUST:
+            if ip.dstip == SERV1:
                 return
             if ip.protocol == ipv4.ICMP_PROTOCOL:
                 return
 
-        # ---------- ROUTING ----------
+        # ---- ROUTING ----
         if ip.dstip not in self.arp_table:
-            return  # Haven't learned yet
+            return  # wait for ARP learning
 
         dst_mac, out_port = self.arp_table[ip.dstip]
 
         # Install flow
-        msg = of.ofp_flow_mod()
-        msg.priority = 10
-        msg.match.dl_type = ethernet.IP_TYPE
-        msg.match.nw_dst = ip.dstip
-        msg.actions.append(of.ofp_action_dl_addr.set_src(ROUTER_MAC))
-        msg.actions.append(of.ofp_action_dl_addr.set_dst(dst_mac))
-        msg.actions.append(of.ofp_action_output(port=out_port))
-        self.connection.send(msg)
+        fm = of.ofp_flow_mod()
+        fm.priority = 100
+        fm.match.dl_type = ethernet.IP_TYPE
+        fm.match.nw_dst = ip.dstip
+        fm.actions.append(of.ofp_action_dl_addr.set_src(ROUTER_MAC))
+        fm.actions.append(of.ofp_action_dl_addr.set_dst(dst_mac))
+        fm.actions.append(of.ofp_action_output(port=out_port))
+        self.connection.send(fm)
 
         # Forward current packet
-        pkt = of.ofp_packet_out()
-        pkt.data = event.ofp
-        pkt.actions.append(of.ofp_action_dl_addr.set_src(ROUTER_MAC))
-        pkt.actions.append(of.ofp_action_dl_addr.set_dst(dst_mac))
-        pkt.actions.append(of.ofp_action_output(port=out_port))
-        self.connection.send(pkt)
+        po = of.ofp_packet_out()
+        po.data = event.ofp
+        po.actions = fm.actions
+        self.connection.send(po)
 
+# ---------------- LAUNCH ----------------
 def launch():
-    def start_switch(event):
+    def start(event):
+        # core switch has 5 ports
         if len(event.connection.ports) == 5:
             Part4Router(event.connection)
         else:
+            # edge switches flood
             msg = of.ofp_flow_mod()
             msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
             event.connection.send(msg)
 
-    core.openflow.addListenerByName("ConnectionUp", start_switch)
+    core.openflow.addListenerByName("ConnectionUp", start)
