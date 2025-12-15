2ad8d9996d12221a7936f217b2c30769530f48b7
LamDDT18 <ddtlam000@gmail.com>
2025-12-15 22:38:11 +0700

    Update part3controller.py

--- 461_mininet/pox/part3controller.py
+++ 461_mininet/pox/part3controller.py
@@ -4,7 +4,7 @@ from pox.lib.packet import ethernet, ipv4
 
 log = core.getLogger()
 
-# ---- IP CONSTANTS ----
+
 HNOTRUST_IP = "172.16.10.100"
 SERV1_IP    = "10.0.4.10"
 
@@ -20,27 +20,20 @@ class Part3Controller(object):
         # CORE switch
         if dpid == 21:
             self.setup_core()
-        # ACCESS switches
+        
         else:
             self.setup_access()
 
-    # -------------------------------------------------
-    # ACCESS SWITCH: simple L2 flooding
-    # -------------------------------------------------
+   
     def setup_access(self):
         fm = of.ofp_flow_mod()
         fm.priority = 1
         fm.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
         self.connection.send(fm)
 
-    # -------------------------------------------------
-    # CORE SWITCH: firewall + static routing
-    # -------------------------------------------------
+    
     def setup_core(self):
 
-        # ---------- FIREWALL RULES ----------
-
-        # Block ICMP from hnotrust to internal + server
         internal_targets = [
             "10.0.1.0/24",
             "10.0.2.0/24",
@@ -57,21 +50,13 @@ class Part3Controller(object):
             fm.match.nw_dst = dst
             self.connection.send(fm)
 
-        # Block ALL IP from hnotrust to serv1
+        
         fm = of.ofp_flow_mod()
         fm.priority = 250
         fm.match.dl_type = ethernet.IP_TYPE
         fm.match.nw_src = HNOTRUST_IP
         fm.match.nw_dst = SERV1_IP
         self.connection.send(fm)
-
-        # ---------- ROUTING RULES ----------
-        # cores21 ports:
-        # 1 → s1 (10.0.1.0/24)
-        # 2 → s2 (10.0.2.0/24)
-        # 3 → s3 (10.0.3.0/24)
-        # 4 → dcs31 (10.0.4.0/24)
-        # 5 → hnotrust subnet (172.16.10.0/24)
 
         routes = {
             "10.0.1.0/24": 1,
