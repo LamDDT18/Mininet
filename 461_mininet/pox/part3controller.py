ea1f7fb311b3ad9fca228446c2d35b0d01112d6c
LamDDT18 <ddtlam000@gmail.com>
2025-12-15 19:50:01 +0700

    Update part3controller.py

--- 461_mininet/pox/part3controller.py
+++ 461_mininet/pox/part3controller.py
@@ -4,44 +4,56 @@ from pox.lib.packet import ethernet, ipv4
 
 log = core.getLogger()
 
-CORE_DPID = 21
+HNOTRUST_IP = "172.16.10.100"
+SERV1_IP = "10.0.4.10"
 
 class Part3Controller(object):
     def __init__(self, connection):
         self.connection = connection
         connection.addListeners(self)
 
-        dpid = connection.dpid
+        # Detect core switch by number of ports
+        if len(connection.ports) != 5:
+            self._install_flood_rule()
+        else:
+            self._install_core_rules()
 
-        # Non-core switches: flood everything
-        if dpid != CORE_DPID:
-            msg = of.ofp_flow_mod()
-            msg.priority = 1
-            msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
-            connection.send(msg)
-            return
+    def _install_flood_rule(self):
+        msg = of.ofp_flow_mod()
+        msg.priority = 1
+        msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+        self.connection.send(msg)
 
-        # ---------- CORE SWITCH (cores21) ----------
+    def _install_core_rules(self):
+        # ---------- SECURITY POLICIES ----------
 
-        # Block ICMP from hnotrust1 to internal + server
-        for dst in ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.10/32"]:
+        # Block ICMP from hnotrust to internal + server
+        internal_subnets = [
+            "10.0.1.0/24",
+            "10.0.2.0/24",
+            "10.0.3.0/24",
+            "10.0.4.10/32"
+        ]
+
+        for dst in internal_subnets:
             msg = of.ofp_flow_mod()
             msg.priority = 200
             msg.match.dl_type = ethernet.IP_TYPE
             msg.match.nw_proto = ipv4.ICMP_PROTOCOL
-            msg.match.nw_src = "172.16.10.100"
+            msg.match.nw_src = HNOTRUST_IP
             msg.match.nw_dst = dst
-            connection.send(msg)
+            self.connection.send(msg)
 
-        # Block all IP from hnotrust1 to serv1
+        # Block all IP from hnotrust to serv1
         msg = of.ofp_flow_mod()
         msg.priority = 190
         msg.match.dl_type = ethernet.IP_TYPE
-        msg.match.nw_src = "172.16.10.100"
-        msg.match.nw_dst = "10.0.4.10"
-        connection.send(msg)
+        msg.match.nw_src = HNOTRUST_IP
+        msg.match.nw_dst = SERV1_IP
+        self.connection.send(msg)
 
-        # Forwarding rules (static)
+        # ---------- STATIC ROUTING ----------
+
         routes = {
             "10.0.1.0/24": 1,
             "10.0.2.0/24": 2,
@@ -56,7 +68,7 @@ class Part3Controller(object):
             msg.match.dl_type = ethernet.IP_TYPE
             msg.match.nw_dst = subnet
             msg.actions.append(of.ofp_action_output(port=port))
-            connection.send(msg)
+            self.connection.send(msg)
 
 def launch():
     def start_switch(event):
